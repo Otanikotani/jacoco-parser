609181387ff5ba94fe3d948901485a775d5a3372
Andrey Artyukhov <otanikotani@gmail.com>
2017-03-28 10:54:04 +0700

    Add multiple modules to index Increment version to 2.0.3

--- src/main/java/com/aurea/jacoco/JacocoIndex.java
+++ src/main/java/com/aurea/jacoco/JacocoIndex.java
@@ -2,15 +2,33 @@ package com.aurea.jacoco;
 
 import com.aurea.jacoco.unit.ModuleCoverage;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Stream;
+
 public class JacocoIndex {
 
-    private final ModuleCoverage moduleCoverage;
+    private final List<ModuleCoverage> moduleCoverages;
 
     public JacocoIndex(ModuleCoverage moduleCoverage) {
-        this.moduleCoverage = moduleCoverage;
+        moduleCoverages = new ArrayList<>();
+        moduleCoverages.add(moduleCoverage);
+    }
+
+    public List<ModuleCoverage> getModuleCoverages() {
+        return moduleCoverages;
+    }
+
+    public Stream<ModuleCoverage> moduleCoverages() {
+        return getModuleCoverages().stream();
+    }
+
+    public JacocoIndex addModuleCoverage(ModuleCoverage moduleCoverage) {
+        getModuleCoverages().add(moduleCoverage);
+        return this;
     }
 
     public ModuleCoverage getModuleCoverage() {
-        return moduleCoverage;
+        return moduleCoverages.get(0);
     }
 }
