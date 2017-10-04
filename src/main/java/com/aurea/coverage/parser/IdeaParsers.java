9c5e328772eb25727d8f7edd7619416cde656822
Andrey Artyukhov <andrey.artyukhov@aurea.com>
2017-10-04 15:37:21 +0700

    Archive html is unsupported so far

--- src/main/java/com/aurea/coverage/parser/IdeaParsers.java
+++ src/main/java/com/aurea/coverage/parser/IdeaParsers.java
@@ -1,10 +1,8 @@
 package com.aurea.coverage.parser;
 
 import com.aurea.coverage.CoverageIndex;
-import com.aurea.coverage.parser.idea.ArchiveHtmlReportParser;
 import com.aurea.coverage.parser.idea.HtmlReportParser;
 
-import java.io.InputStream;
 import java.nio.file.Path;
 
 public final class IdeaParsers {
